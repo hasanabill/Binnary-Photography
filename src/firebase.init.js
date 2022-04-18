485bbc3384f7a9edf1d4d4c535f46ca6240f1a38
hasanabil <mahmudul.anabil2862@gmail.com>
2022-04-18 23:11:33 +0600

    environment variable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCAHoble3zSlQasyDd6KUkGs5tydGArrfc",
-    authDomain: "binnary-photography.firebaseapp.com",
-    projectId: "binnary-photography",
-    storageBucket: "binnary-photography.appspot.com",
-    messagingSenderId: "310236899833",
-    appId: "1:310236899833:web:2e9228362e7d6c55e0f528"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
